5f667e620af46656efd211b378e7caec58306121
Michael JasonSmith <mpj17@onlinegroups.net>
2015-02-04 11:24:36 -0800

    The basics of the traversal

--- gs/group/messages/export/view.py
+++ gs/group/messages/export/view.py
@@ -17,12 +17,15 @@ from json import dumps as dump_json
 from logging import getLogger
 log = getLogger('gs.group.messages.export')
 from zope.cachedescriptors.property import Lazy
+from zope.component import getMultiAdapter
 from gs.core import curr_time
 from gs.group.base import GroupPage
+from gs.group.list.email.text.post import Post
 from .queries import PostsQuery
 
 
 class PostsJSON(GroupPage):
+    '''The list of posts in a given month, in JSON format'''
 
     @Lazy
     def yearMonth(self):
@@ -45,4 +48,49 @@ class PostsJSON(GroupPage):
         posts = self.query.posts_for_month(self.siteInfo.id,
                                            self.groupInfo.id, year, month)
         retval = dump_json(posts)
+        return retval
+
+
+class MessageTraversal(GroupPage):
+    '''The "traversal" system for the mbox view of a post
+
+:param messages: The messages folder for a group.
+:type messages: Products.XWFMailingListManager.interfaces.IGSMessagesFolder
+:param request: The request-object.
+'''
+    def __init__(self, messages, request):
+        super(MessageTraversal, self).__init__(messages, request)
+        self.traverse_subpath = []
+        self.post = None
+
+    def publishTraverse(self, request, name):
+        '''Traverse through the path
+
+The first time this method is called it loads the post referenced by
+``name``. Subsequent calls just append ``name`` to the ``traverse_subpath``
+property.'''
+        self.traverse_subpath.append(name)
+        if self.post is None:
+            # Load the post, and hook it into the aquisition tree
+            self.post = Post(self.context, self.groupInfo, name)
+        return self
+
+    def __call__(self):
+        '''The traversal is done, render something.
+
+Get a named adapter for the post, and the request, defaulting to the
+``mbox`` name. Call the adapter, returning the result.'''
+        tsp = self.traverse_subpath
+        name = tsp[1] if len(tsp) > 1 else 'mbox'
+        page = getMultiAdapter((self.post, self.request),
+                               name=name)
+        retval = page()
+        return retval
+
+
+class MBox(GroupPage):
+    def __call__(self):
+        self.request.response.setHeader(b'Content-Type',
+                                        b'text/plain; charset=utf-8')
+        retval = 'Foo'
         return retval
