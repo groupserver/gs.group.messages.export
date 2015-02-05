d27b9085306d466e45225f32115a7ce23839f580
Michael JasonSmith <mpj17@onlinegroups.net>
2015-02-04 17:20:47 -0800

    Refactoring into two files

--- gs/group/messages/export/view.py
+++ gs/group/messages/export/view.py
@@ -13,18 +13,11 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals, print_function
-from email import encoders
-from email.parser import Parser
-from email.mime.multipart import MIMEMultipart
-from email.mime.nonmultipart import MIMENonMultipart
-from email.mime.text import MIMEText
-from email.utils import parseaddr
 from json import dumps as dump_json
 from logging import getLogger
 log = getLogger('gs.group.messages.export')
 from zope.cachedescriptors.property import Lazy
 from zope.component import getMultiAdapter
-from gs.content.email.base import TextMixin
 from gs.core import curr_time
 from gs.group.base import GroupPage
 from gs.group.list.email.text.post import Post
@@ -99,84 +92,4 @@ Get a named adapter for the post, and the request, defaulting to the
         page = getMultiAdapter((self.post, self.request),
                                name=name)
         retval = page()
-        return retval
-
-
-class GSMime(MIMENonMultipart):
-    def __init__(self, _data, _type, _subtype,
-                 _encoder=encoders.encode_base64, **_params):
-        MIMENonMultipart.__init__(self, _type, _subtype, **_params)
-        self.set_payload(_data)
-        _encoder(self)
-
-
-class MBox(GroupPage, TextMixin):
-    @Lazy
-    def post(self):
-        return self.context.post
-
-    @Lazy
-    def bodyPayload(self):
-        retval = MIMEMultipart('alternative')
-        t = MIMEText(self.post['body'], 'plain', 'utf-8')
-        retval.attach(t)
-        if self.post['htmlbody']:
-            h = MIMEText(self.post['htmlbody'], 'html', 'utf-8')
-            retval.attach(h)
-        return retval
-
-    @Lazy
-    def fileArchive(self):
-        groupObj = self.context.context
-        retval = getattr(groupObj, 'files')
-        return retval
-
-    @Lazy
-    def payload(self):
-        if self.post['has_attachments']:
-            retval = MIMEMultipart('mixed')
-            retval.attach(self.bodyPayload)
-            for f in self.post['files_metadata']:
-                mainType, subtype = f['mime_type'].split('/')
-                data = self.fileArchive.get_file_by_id(f['file_id'])
-                if data:
-                    binaryData = str(data)
-                    m = GSMime(binaryData, mainType, subtype,
-                               filename=f['file_name'])
-                    retval.attach(m)
-        else:
-            retval = self.bodyPayload
-        return retval
-
-    @staticmethod
-    def unixfrom(fromHdr, date):
-        fromAddr = parseaddr(fromHdr)[1]
-        r = 'From {0} {1}'
-        retval = r.format(fromAddr, date)
-        return retval
-
-    def __call__(self):
-        fn = b'{0}-{1}-post-{2}.mbox'.format(
-            self.siteInfo.id, self.groupInfo.id, self.post['post_id'])
-        self.request.response.setHeader(b'Content-Disposition',
-                                        b'inline; filename='+fn)
-        self.request.response.setHeader(b'Content-type',
-                                        b'text/plain; charset=utf-8')
-
-        m = self.post['header'] + '\n\nDiscarded'
-        e = Parser().parsestr(m, headersonly=True)
-        if 'Content-Transfer-Encoding' in e:
-            del(e['Content-Transfer-Encoding'])
-
-        outMsg = self.payload
-        for h, v in e.items():
-            if outMsg[h]:
-                pass
-            else:
-                outMsg[h] = v
-        dateStr = self.post['date'].strftime('%a, %d %b %Y %H:%M:%S %z')
-        outMsg['Date'] = dateStr
-        outMsg.set_unixfrom(self.unixfrom(e['from'], dateStr))
-
-        retval = outMsg.as_string(unixfrom=True)
         return retval
