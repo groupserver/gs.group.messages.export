173a00b9066953da3ac21db7e26c420f62ae7ab4
Michael JasonSmith <mpj17@onlinegroups.net>
2015-09-06 10:54:11 -0700

    Metadata update

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.group.messages.export'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -27,7 +28,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.group.messages.export',
+    name=name,
     version=version,
     description="Export messages from a GroupServer group",
     long_description=long_description,
@@ -46,10 +47,11 @@ setup(
     keywords='groupserver message',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.group.messages.export',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
